86e631eda4bb4b5bd1dd7c3a70e77043fef3c886
Nitin Sapra <codedev44@gmail.com>
2025-12-14 17:41:24 +0530

    Fixed Bg

--- src/app/components/AppViewport.jsx
+++ src/app/components/AppViewport.jsx
@@ -2,19 +2,38 @@
 
 export default function AppViewport({ children }) {
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        overflow: "hidden",
-      }}
-    >
-      <div className="mx-auto max-w-[560px] w-full h-full relative">
-        {children}
+    <>
+      {/* Background image that extends behind Dynamic Island */}
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          backgroundImage: "url('/placeholder_01_1080x1920.jpeg')",
+          backgroundSize: "cover",
+          backgroundPosition: "top center",
+          backgroundRepeat: "no-repeat",
+          zIndex: -1,
+        }}
+      />
+      
+      {/* Main content container */}
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          overflow: "hidden",
+        }}
+      >
+        <div className="mx-auto max-w-[560px] w-full h-full relative">
+          {children}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -38,10 +38,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
-      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${firaSans.variable} antialiased`}
       >
