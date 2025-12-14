e845fe4adaa9031e7ab3560c1aa9559c6872f5c2
Nitin Sapra <codedev44@gmail.com>
2025-12-14 17:36:01 +0530

    Added background

--- src/app/components/AppViewport.jsx
+++ src/app/components/AppViewport.jsx
@@ -3,18 +3,18 @@
 export default function AppViewport({ children }) {
   return (
     <div
-      className="mx-auto max-w-[560px] w-full relative overflow-hidden"
       style={{
         position: "fixed",
         top: 0,
         left: 0,
         right: 0,
         bottom: 0,
-        height: "100%",
-        width: "100%",
+        overflow: "hidden",
       }}
     >
-      {children}
+      <div className="mx-auto max-w-[560px] w-full h-full relative">
+        {children}
+      </div>
     </div>
   );
 }
