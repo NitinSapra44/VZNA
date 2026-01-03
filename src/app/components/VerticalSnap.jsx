6433e39591470db497596508ebef626d382d7a14
Nitin Sapra <codedev44@gmail.com>
2026-01-04 00:52:39 +0530

    speed

--- src/app/components/VerticalSnap.jsx
+++ src/app/components/VerticalSnap.jsx
@@ -123,7 +123,7 @@ export default function VerticalSnap({ children, isDrawerOpen }) {
       direction="vertical"
       modules={[Virtual, Mousewheel]}
       slidesPerView={1}
-      speed={250}
+      speed={200}
       virtual={{
         enabled: true,
         addSlidesBefore: 2,
